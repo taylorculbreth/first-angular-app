9407c07d62f6e8a6bfd93b0be8caeb7cf9e9216d
Taylor S. Culbreth <tsculbreth@pilotcat.com>
2017-07-07 16:07:11 -0500

    built in directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styles: [`
+    .online{
+      color: white;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test server';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,6 +27,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
